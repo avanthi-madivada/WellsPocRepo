d4664e7436ef0f4aa42b0e20861d8bdffe9a9b43
40007275 <40007275@BLTSL02179.lnties.com>
2020-06-18 01:48:08 -0700

    added command for show details and handler class

---
+++ com.ltts.wellspoc/src/com/ltts/wellspoc/WellDetailsHandler.java
@@ -0,0 +1,23 @@
+package com.ltts.wellspoc;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class WellDetailsHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+		    HandlerUtil.getActiveWorkbenchWindow(event)
+		.getActivePage()
+		.showView("com.ltts.wellspoc.welldetailsview");
+		    } catch (PartInitException e) {
+		        throw new ExecutionException("Error while opening view", e);
+		    }
+		    return null;
+	}
+
+}
