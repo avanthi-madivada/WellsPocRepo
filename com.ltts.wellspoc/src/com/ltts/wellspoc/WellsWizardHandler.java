c6774730caf5c8f21fed09af25ce23bc52ebb57d
40006037 <40006037@MYTSP01982.lnties.com>
2020-06-23 18:43:44 +0530

    UI Change - disabled maximize option.

--- com.ltts.wellspoc/src/com/ltts/wellspoc/WellsWizardHandler.java
+++ com.ltts.wellspoc/src/com/ltts/wellspoc/WellsWizardHandler.java
@@ -1,15 +1,12 @@
 package com.ltts.wellspoc;
 
-import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.wizard.IWizard;
+
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 
 import com.ltts.wellspoc.ui.wizard.WellsWizard;
 
@@ -25,13 +22,10 @@ public class WellsWizardHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		WizardDialog wizardDialog = new WizardDialog(Display.getDefault().getActiveShell(), new WellsWizard());
-		wizardDialog.setPageSize(600, 400); 
-		wizardDialog.setShellStyle(SWT.CLOSE |SWT.TITLE | SWT.BORDER );
+		wizardDialog.setPageSize(600, 400);
+		wizardDialog.setShellStyle(SWT.CLOSE | SWT.TITLE | SWT.BORDER);
 		wizardDialog.open();
 		return null;
 	}
-	
 
 }
-
-
