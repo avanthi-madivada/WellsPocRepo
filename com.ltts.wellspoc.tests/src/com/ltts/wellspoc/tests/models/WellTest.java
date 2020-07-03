811bdd1c5d2fa538b2479e501707651ac58ddfbb
40007275 <40007275@BLTSL02179.lnties.com>
2020-07-03 03:41:32 -0700

    added test for other fields in well class

--- com.ltts.wellspoc.tests/src/com/ltts/wellspoc/tests/models/WellTest.java
+++ com.ltts.wellspoc.tests/src/com/ltts/wellspoc/tests/models/WellTest.java
@@ -31,5 +31,62 @@ public class WellTest extends TestCase {
 		assertNotNull(wellInstance.getWellPlanName());
 		assertEquals("Here is the test for well name attribute", "Well 1", wellInstance.getWellPlanName());
 	}
+	
+	@Test
+	public void test_getWellEasting() {
+		
+		assertNotNull(wellInstance.getEasting());
+		//assertTrue(checkForPositiveNumber(wellInstance.getEasting()));
+		assertEquals("Test for easting attribute",checkForPositiveNumber(wellInstance.getEasting()),420107.6 );
+	}
+	
+	//checking for positive number
+	public Double checkForPositiveNumber(Double easting) {
+		if(easting>0 && easting %1 !=0)
+			{
+			return easting;
+			}
+		else 
+			return 0.0;
+		}
+	
+	@Test
+	public void test_getWellNorthing() {
+		
+		assertNotNull(wellInstance.getNorthing());
+		//assertTrue(checkForPositiveNumber(wellInstance.getEasting()));
+		assertEquals("Test for northing attribute",checkForPositiveNumber(wellInstance.getNorthing()),7244305.1);
+	}
+	
+	@Test
+	public void test_getWellAzimuth() {
+		
+		assertNotNull(wellInstance.getAzimuth());
+		assertTrue(checkForAzimuthRange(wellInstance.getAzimuth()));
+		assertEquals("Test for azimuth attribute",240.0,wellInstance.getAzimuth());
+	}
+
+	//check if the angle is between 0 and 360
+	private boolean checkForAzimuthRange(Double angle) {
+		if(angle>0&&angle<360)
+			{
+			return true;
+			}
+		else {
+			return false;
+		}
+	}
+	
+	@Test
+	public void test_getWellFieldName() {
+		assertNotNull(wellInstance.getField());
+		assertEquals("Test for well field attribute", "Ghawar", wellInstance.getField());
+	}
+	
+	@Test
+	public void test_getWellType() {
+		assertNotNull(wellInstance.getType());
+		assertEquals("Test for well Type attribute", "Horizontal", wellInstance.getType());
+	}
 
 }
