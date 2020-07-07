8ab33e2680795ab9717e83167a41317e6f1e3e1d
40007275 <40007275@BLTSL02179.lnties.com>
2020-07-06 23:32:13 -0700

    added testcases for other fields in well model

--- com.ltts.wellspoc.tests/src/com/ltts/wellspoc/tests/models/WellTest.java
+++ com.ltts.wellspoc.tests/src/com/ltts/wellspoc/tests/models/WellTest.java
@@ -13,7 +13,11 @@ import junit.framework.TestCase;
 public class WellTest extends TestCase {
 
 	private Well wellInstance;
-
+	public static boolean isValid = true;
+	// Min and Max value for azimuth
+	Double minValue = 1.0;
+	Double maxValue = 360.0;
+	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -36,46 +40,41 @@ public class WellTest extends TestCase {
 	public void test_getWellEasting() {
 		
 		assertNotNull(wellInstance.getEasting());
-		//assertTrue(checkForPositiveNumber(wellInstance.getEasting()));
-		assertEquals("Test for easting attribute",checkForPositiveNumber(wellInstance.getEasting()),420107.6 );
+		assertTrue(checkIfNumber(wellInstance.getEasting()));
+		assertEquals("Test for easting attribute",wellInstance.getEasting(),420107.6 );
 	}
 	
-	//checking for positive number
-	public Double checkForPositiveNumber(Double easting) {
-		if(easting>0 && easting %1 !=0)
+	//checking if entered  number is positive
+	public boolean checkIfNumber(Double value) {
+		try {
+			if(value>0&& value%1 !=0)
 			{
-			return easting;
-			}
-		else 
-			return 0.0;
+				isValid=true;
+				return isValid;		
+			}		 
+		} catch (NumberFormatException exception) {
+			isValid = false;		
+			return isValid;
 		}
+		return isValid;
+	}
 	
 	@Test
 	public void test_getWellNorthing() {
 		
 		assertNotNull(wellInstance.getNorthing());
-		//assertTrue(checkForPositiveNumber(wellInstance.getEasting()));
-		assertEquals("Test for northing attribute",checkForPositiveNumber(wellInstance.getNorthing()),7244305.1);
+		assertTrue(checkIfNumber(wellInstance.getNorthing()));
+		assertEquals("Test for northing attribute",wellInstance.getNorthing(),7244305.1);
 	}
 	
 	@Test
 	public void test_getWellAzimuth() {
 		
 		assertNotNull(wellInstance.getAzimuth());
-		assertTrue(checkForAzimuthRange(wellInstance.getAzimuth()));
+		assertTrue(checkForAzimuthRange(wellInstance.getAzimuth(), minValue, maxValue));
 		assertEquals("Test for azimuth attribute",240.0,wellInstance.getAzimuth());
 	}
-
-	//check if the angle is between 0 and 360
-	private boolean checkForAzimuthRange(Double angle) {
-		if(angle>0&&angle<360)
-			{
-			return true;
-			}
-		else {
-			return false;
-		}
-	}
+	
 	
 	@Test
 	public void test_getWellFieldName() {
@@ -89,4 +88,24 @@ public class WellTest extends TestCase {
 		assertEquals("Test for well Type attribute", "Horizontal", wellInstance.getType());
 	}
 
+	
+	//check if the angle is between 0 and 360
+		private boolean checkForAzimuthRange(Double value,Double minValue, Double maxValue) {
+			checkIfNumber(value);
+			try {
+				Double enteredValue = Double.valueOf(value);
+				if (enteredValue < minValue || enteredValue > maxValue) {		
+					isValid= false;
+					return isValid;
+				} 
+				else {
+					isValid= true;
+					return isValid;
+				}
+			} catch (NumberFormatException exception) {
+				return false;
+			}
+		}
+	
+	
 }
