a32f4aa483f835eb6eb86fa5b6771ef15b2a1bed
40008752 <40008752@BLRMIESPC-1141.lnties.com>
2020-07-22 19:56:11 +0530

    UserModelTest and MessagesUtil test cases added into same branch and comitted code into git.

---
+++ com.ltts.wellspoc.tests/src/com/ltts/wellspoc/tests/models/UserModelTest.java
@@ -0,0 +1,63 @@
+package com.ltts.wellspoc.tests.models;
+
+import org.junit.Test;
+
+import com.ltts.wellspoc.models.UserModel;
+
+import junit.framework.TestCase;
+
+/**
+ * Class with tests for all the attributes of UserModel.java.
+ * 
+ * @author Padmaja
+ *
+ */
+
+public class UserModelTest extends TestCase {
+
+	private UserModel userInstance;
+	private UserModel setUserInstance;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		userInstance = new UserModel("admin", "admin");
+
+		setUserInstance = new UserModel();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	public void test_getUserName() {
+		assertNotNull(userInstance.getUserName());
+		assertEquals("Here is the test for UserName", "admin", userInstance.getUserName());
+	}
+
+	@Test
+	public void test_getPassword() {
+
+		assertNotNull(userInstance.getPassword());
+		assertEquals("Here is the test for Password", "admin", userInstance.getPassword());
+	}
+
+	// setter test cases
+
+	@Test
+	public void test_setUserName() {
+		String userName = "admin";
+		setUserInstance.setUserName(userName);
+		assertEquals("Test for UserName attribute", userName, setUserInstance.getUserName());
+	}
+
+	@Test
+	public void test_setPassword() {
+		String password = "admin";
+
+		setUserInstance.setPassword(password);
+		assertEquals("Test for PassWord attribute", password, setUserInstance.getPassword());
+	}
+}
---
+++ com.ltts.wellspoc.tests/src/com/ltts/wellspoc/tests/models/ui/util/MessagesUtilTest.java
@@ -0,0 +1,67 @@
+package com.ltts.wellspoc.tests.models.ui.util;
+
+import org.junit.Test;
+import com.ltts.wellspoc.ui.util.MessagesUtil;
+import junit.framework.TestCase;
+
+/**
+ * Class with tests for methods of MessagesUtil.java.
+ * 
+ * @author padmaja
+ *
+ */
+public class MessagesUtilTest extends TestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	public void test_checkIfNumberValid() {
+		boolean isValid = true;
+		boolean actualValue;
+		String text = "4000";
+		assertNotNull(text);
+		actualValue = MessagesUtil.checkIfNumber(text);
+
+		assertEquals("Testcase for checkIfNumber method", isValid, actualValue);
+
+	}
+
+	@Test
+	public void test_checkInLimit() {
+		boolean isValid = true;
+		boolean actualValue;
+		String text = "4000";
+
+		Double minValue = 24.00;
+		Double maxValue = 250.00;
+
+		assertNotNull(text);
+		actualValue = MessagesUtil.checkInLimit(text, minValue, maxValue);
+		assertEquals("Testcase for checkInLimit method", isValid, actualValue);
+
+	}
+
+	@Test
+	public void test_restrictEnteredChars() {
+		boolean isValid = true;
+		boolean actualValue;
+		String text = "4000";
+		Double minValue = 12.00;
+		Double maxValue = 350.00;
+
+		assertNotNull(text);
+		actualValue = MessagesUtil.restrictEnteredChars(text, minValue, maxValue);
+		assertEquals("Testcase  for restrictEnteredChars method ", isValid, actualValue);
+
+	}
+
+}
