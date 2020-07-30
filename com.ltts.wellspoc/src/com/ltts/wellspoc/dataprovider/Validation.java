cd861ebf733d11d1040ee58a8168217401cebfc8
40008752 <40008752@BLRMIESPC-1141.lnties.com>
2020-07-30 20:11:44 +0530

    comitted code into git.

--- com.ltts.wellspoc/src/com/ltts/wellspoc/dataprovider/Validation.java
+++ com.ltts.wellspoc/src/com/ltts/wellspoc/dataprovider/Validation.java
@@ -5,16 +5,24 @@ import java.util.List;
 import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
 import org.eclipse.nebula.widgets.nattable.data.validate.DataValidator;
 import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
 
 import com.ltts.wellspoc.models.Well;
 import com.ltts.wellspoc.models.WellDataProvider;
 import com.ltts.wellspoc.ui.addnewwell.AddNewWellModelMgr;
+import com.ltts.wellspoc.ui.util.MessagesUtil;
 
 public class Validation extends DataValidator {
 
 	private List<Well> wellData = WellDataProvider.wellDataProvider.getWell();
 
 	private String cellData;
+	private static final Device device = Display.getCurrent();
+	private static final Color red = new Color(device, 255, 0, 0);
+	private final static Color black = new Color(device, 0, 0, 0);
 
 	@Override
 	public boolean validate(ILayerCell cell, IConfigRegistry configRegistry, Object newValue) {
@@ -39,22 +47,49 @@ public class Validation extends DataValidator {
 				}
 			}
 		}
-		if (columnIndex == 3) {
-			if ((((Double) newValue).doubleValue() <= 360)) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			if ((newValue instanceof Double) && (((Double) newValue).doubleValue() > 10000)) {
-				return true;
 
-			} else if (newValue instanceof String) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
+		// for azimuth validation
+				if (columnIndex == 3) {
+					if ((((Double) newValue).doubleValue() < 0) || (((Double) newValue).doubleValue() > 360)) {
 
+						return false;
+					} else {
+
+						return true;
+					}
+				}
+
+				
+				
+
+		// for easting validation
+
+				if (columnIndex == 1) {
+
+					if (MessagesUtil.restrictEnteredChars(newValue.toString(), Double.MIN_VALUE, Double.MAX_VALUE)) {
+						System.out.println("" + newValue.toString());
+					
+						return true;
+					} else {
+
+						return false;
+					}
+
+				}
+				return false;
+
+			}
+
+			// for testing northing
+
+			public void CheckValue( Text text, Double minValue, Double maxValue) {
+				if (MessagesUtil.restrictEnteredChars(text.getText().toString(), minValue, maxValue)) {
+				
+					text.setForeground(black);
+
+				} else {
+					text.setForeground(red);
+				}
+
+			}
 }
